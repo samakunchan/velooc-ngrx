6e5763260dd20f2c55a67c370d30f4d9ff41bd22
samakunchan <badgehades@hotmail.com>
2020-06-24 11:46:41 +0200

    feat: ngrx
     Installation du module "@ngrx/data"
     Installation du module "@ngrx/effects"
     Installation du module "@ngrx/entity"
     Installation du module "@ngrx/router-store"
     Installation du module "@ngrx/schematics"
     Installation du module "@ngrx/store"
     Installation du module "@ngrx/store-devtools"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EntityDataModule } from '@ngrx/data';
+import { entityConfig } from './entity-metadata';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,6 +26,11 @@ import { AppRoutingModule } from './app-routing.module';
     MatToolbarModule,
     RouterModule,
     AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    // EntityDataModule.forRoot(entityConfig),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
