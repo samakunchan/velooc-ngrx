3e653f7c5f0ebd4c65d64cbde68a62a8e4840f4e
samakunchan <badgehades@hotmail.com>
2020-06-24 10:45:59 +0200

    feat: component
    - Ajout home component + scss + html
    - Ajout about component + scss + html
    - Ajout map component + scss + html

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'velooc-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
